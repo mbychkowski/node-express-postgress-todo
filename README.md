4fc783b93c9db8d33cc6a9c34f3ac932a13a918b
michael bychkowski <mbychkowski@gmail.com>
2018-09-08 22:43:43 -0400

    setup boiler plate for todo app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # node-express-postgress-todo
-A simple CRUD application built with Node, Express, and PostgreSQL.
+A simple CRUD application built with Node, Express, and PostgreSQL. Boiler plate taken from [**mjhea0 (Michael Herman)**](https://github.com/mjhea0/node-postgres-sequelize)
---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models/index');
+
+//Routes
+
+// GET all all todos
+router.get()
+
+// GET single todo
+router.get()
+
+// POST new todo
+router.post()
+
+// UPDATE single todo
+router.put()
+
+// DELETE single todo
+router.delete();
